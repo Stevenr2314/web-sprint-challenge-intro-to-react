85bf5058f8b05b58d951730047384d0b4fbafcff
Steven Rosa <s.rosa21@icloud.com>
2021-11-05 18:28:51 -0400

    Succesfully displayed necessary information

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,20 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(resp => {
+      setCharacters(resp.data)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characters={characters}></Character>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,81 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const CharacterWrapper = styled.div`
+    display: flex;
+`;
+
+const ImageWrapper = styled.span`
+    display: flex;
+`;
+
+const ImageButton = styled.span`
+    display: inline-block;
+`;
+
+const CharacterImage = styled.span`
+    border-radius: 2%;
+`;
+
+const CharacterInfo = styled.ul`
+    display: flex;
+    flex-direction: column;
+    list-style-type: none;
+`;
+
+const KeyP = styled.p`
+    color: 'red';
+    font-weight: 600;
+    display: inline-block;
+`;
+
+const ValueP = styled.p`
+    color: 'blue';
+    display: inline-block;
+`;
+
+const CharInfoDiv = styled.div`
+    background-color: 'antiquewhite';
+    list-style-type: none;
+`;
+
+const characterAttr = (character) => {
+    const characterEntries = Object.entries(character).slice(0,7)
+    const listCharacterAttr = characterEntries.map( ([key, value]) => (
+        <li>
+            <KeyP>{key}:</KeyP> <ValueP>{value}</ValueP>
+        </li>
+    ));
+
+    return <ul>{listCharacterAttr}</ul>
+}
+
+const Character = props =>
+{
+    const {characters} = props;
+    /* 
+        Iterate over each character and save each key value pair up to a certain amount
+    
+    */
+    
+
+    return (
+        <CharacterWrapper>
+            <ImageWrapper>
+                <ImageButton></ImageButton>
+                <CharacterImage></CharacterImage>
+                <ImageButton></ImageButton>
+                {characters.map(character => 
+                   (<CharInfoDiv>
+                        <CharacterInfo>
+                            {characterAttr(character)}
+                        </CharacterInfo>
+                    </CharInfoDiv>)
+                )}
+            </ImageWrapper>
+        </CharacterWrapper>
+    );
+}
+
+export default Character;
